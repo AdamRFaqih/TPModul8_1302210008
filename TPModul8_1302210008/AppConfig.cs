44b419134e0af4914841d9b4c6fc3f363fea2f1c
adamrafiffaqih <adamrafif48@gmail.com>
2023-04-09 06:27:14 +0700

    menyelesaikan program dengan runtime configuration

---
+++ TPModul8_1302210008/AppConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.Json;
+namespace TPModul8_1302210008
+{
+	public class AppConfig
+	{
+		public CovidConfig covidConfig;
+		public AppConfig()
+		{
+		}
+
+		public void readConfig()
+		{
+			string text = File.ReadAllText(@"./covid_config.json");
+			covidConfig = JsonSerializer.Deserialize<CovidConfig>(text);
+		}
+
+		public void setDefault()
+		{
+			covidConfig = new CovidConfig("celcius", 14, "Anda tidak diperbolehkan masuk kedalam gedung ini", "Anda dipersilahkan masuk kedalam gedung ini");
+            
+        }
+		public void writeConfig()
+		{
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(covidConfig, options);
+            File.WriteAllText(@"./covid_config.json", jsonString);
+        }
+		public void ubahSatuanSuhu()
+		{
+			if (covidConfig.satuan_suhu == "celcius")
+			{
+				covidConfig.satuan_suhu = "fahreinheit";
+				writeConfig();
+				return;
+			}
+            covidConfig.satuan_suhu = "celcius";
+            writeConfig();
+        }
+    }
+}
+
--- TPModul8_1302210008/Program.cs
+++ TPModul8_1302210008/Program.cs
@@ -1,3 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿using System;
+namespace TPModul8_1302210008
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            AppConfig app = new AppConfig();
+            try
+            {
+                app.readConfig();
+            }
+            catch
+            {
+                app.setDefault();
+                app.writeConfig();
+                app.readConfig();
+            }
+            app.ubahSatuanSuhu();
+            Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {app.covidConfig.satuan_suhu}");
+            double input1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            if ((input1 >= 36.5 && input1 < 37.5 && app.covidConfig.satuan_suhu == "celcius") || (input1 >= 97.7 && input1 < 99.5))
+            {
+                Console.WriteLine(app.covidConfig.pesan_diterima);
+                return;
+            }
+            else if (input2 > app.covidConfig.batas_hari_demam)
+            {
+                Console.WriteLine(app.covidConfig.pesan_diterima);
+                return;
+            }
+            Console.WriteLine(app.covidConfig.pesan_ditolak);
+        }
+    }
+}
